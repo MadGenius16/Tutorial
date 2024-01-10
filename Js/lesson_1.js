978926743f957b18ff8bf2b4e6038b425ab71c70
MadGenius16 <aleksandrco777@gmail.com>
2024-01-10 19:36:06 +0200

    1

---
+++ Js/lesson_1.js
@@ -0,0 +1,52 @@
+ let a = 5;
+ console.log(a);
+
+ a = 10;  
+ console.log(a);
+
+ const b = 3;
+ console.log(b);
+
+//  b=33;
+//  console.log(b);
+
+
+
+let c;
+console.log(c);
+
+
+// Типи даних(примітивні)
+
+// number
+
+// string
+
+// Boolean(bool) (true, false)
+
+// undefined
+
+// null
+
+///////////
+
+const userAge = 15;
+const userName = 'alex';
+const isUserAdmin = false;
+
+// typeof оператор , можна перевірити тип даних значення або змінної
+
+console.log(typeof userAge);
+console.log(typeof userName);
+console.log(typeof isUserAdmin);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof NaN);
+
+// Помилки:
+// 1. Звернення до змінних або констант до моменту створення(оголошення)
+// 2. константа не змінна
+// 3. звертання до неправильной змінной (букви повинні бути обднакові)
+
+console.log()
+

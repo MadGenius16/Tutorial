e5fbe6d21b30075ca38a6a3b093c411d40338c16
MadGenius16 <aleksandrco777@gmail.com>
2024-01-19 20:21:29 +0200

    Update lesson_3.js

--- Js/lesson_3.js
+++ Js/lesson_3.js
@@ -49,5 +49,92 @@ for (let num=0; num<5; num++) {
   
 }
 
+// методи рядків
+
+const username = "Jacob Mercer";
+
+// slice(start, end) метод для рядків який копіює і повертає пядрядок
+// не змінюючи його
+
+// start, end - індекси що відповідають за початок і кінець копіювання 
+
+// 'hello'.slice(1, 3)-[1,3)- el
+// 'hello'.slice(3)-[1,3)- lo
+// 'hello'.slice(-1)- o(остання буква 'o')
+
+
+const firstName = username.slice(0, 5)
+const lastName = username.slice(6)
+console.log(firstName)
+console.log(lastName)
+
+
+
+// toLowerCase/toUpperCase/
+
+const userMsg= 'Hello mY nAMe is JacoB!'
+console.log(userMsg.toLowerCase())
+console.log(userMsg.toUpperCase())
+
+
+// includes(string, position) перевіряє чи є підрядок string в рядку
+// на якому викликається метод. Якщо потрібно шукати підрядок починаючи
+// з певного індксу, можна вказати цей індекс другим параметром position.
+
+const userStack = 'hello, I know HTML, CSS, JS, Node. Js'
+const isUserknowHTML = userStack.includes("HTML")
+console.log(`HTML: ${isUserknowHTML}`)
+const isUserknowAngular = userStack.includes("Angular")
+console.log(`Angular: ${isUserknowAngular}`)
+
+
+// startsWith/endswith - перевіряють чи починаються/закінч. рядок 
+// символом або іншим рядком. поверт. true / false
+
+const url = 'https://example.com'
+
+console.log(`site ${url} safty status ${isUrlSafe(url) }`);
+
+function isUrlSafe(url) {
+  return url.startsWith('https')
+}
+
+
+// indexOf(string)- повертає індекс початку підрядка string у рядку
+// на якому викликається. Якщо такого підрядка не має повертає -1
+
+const text='Hallo everyone. My name is Sean'
+const firstSentence = getTextBeforeDivider(text, ".")
+console.log(firstSentence)
+
+function getTextBeforeDivider(text, divider) {
+  const index = text.indexOf(divider)
+  
+  if(index===-1){
+    console.log('такого не існує')
+    return ""
+  }
+  return text.slice(0, index + 1)
+}
+
+// trim() -повертає новий рядок у якому видалені зайві 
+// пробіли на початку і у кінцірядку
+
+const firstNAme = '    John    '
+console.log(`Hello ${firstNAme.trim()}!`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
